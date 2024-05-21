6cd535e4eea5434d3f9404e92fc25f12be348348
Marcus Rådell <marcus@radell.net>
2024-05-21 15:11:23 +0200

    add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+const mockUrls = [
+  "https://utfs.io/f/27f932f1-e5b2-473c-a6e2-362d90de9c58-1d.png",
+  "https://utfs.io/f/7d8db255-2df5-4a27-91df-5f283faecb28-guob9h.jpg",
+  "https://utfs.io/f/86ec96a7-93ce-4b81-9d1f-d956f9fe11a4-5ffa44.png",
+  "https://utfs.io/f/9e26a86e-87c5-467e-bf41-958d310b9e4a-kw9o2f.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Gallery in progress)
+    <main>
+      {" "}
+      <div className="flex flex-wrap">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="Gallery image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
