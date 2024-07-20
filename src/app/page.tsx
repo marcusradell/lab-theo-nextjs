b3a4879627d7e97af90dcd7a2fdd4eed5f43c76e
Marcus Rådell <marcus@radell.net>
2024-07-20 19:29:09 +0200

    use database images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,22 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/86ec96a7-93ce-4b81-9d1f-d956f9fe11a4-5ffa44.png",
-  "https://utfs.io/f/9e26a86e-87c5-467e-bf41-958d310b9e4a-kw9o2f.png",
-];
-
-const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  let images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  images = [...images, ...images, ...images];
 
   return (
     <main>
-      {posts.map((post) => (
-        <div key={post.id}>{post.name}</div>
-      ))}
       <div className="flex flex-wrap gap-4">
-        {[
-          ...mockImages,
-          ...mockImages,
-          ...mockImages,
-          ...mockImages,
-          ...mockImages,
-          ...mockImages,
-        ].map((image, index) => (
+        {images.map((image, index) => (
           <div key={index} className="w-48">
-            <img src={image.url} />
+            {
+              // eslint-disable-next-line @next/next/no-img-element
+              <img src={image.url} alt={image.name} />
+            }
           </div>
         ))}
       </div>
