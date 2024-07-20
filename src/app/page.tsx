21112ef5736c52ca9d9ed1b326fe1e2660a2c0e2
Marcus Rådell <marcus@radell.net>
2024-07-20 10:59:57 +0200

    duplicate mock images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 const mockUrls = [
-  "https://utfs.io/f/27f932f1-e5b2-473c-a6e2-362d90de9c58-1d.png",
-  "https://utfs.io/f/7d8db255-2df5-4a27-91df-5f283faecb28-guob9h.jpg",
   "https://utfs.io/f/86ec96a7-93ce-4b81-9d1f-d956f9fe11a4-5ffa44.png",
   "https://utfs.io/f/9e26a86e-87c5-467e-bf41-958d310b9e4a-kw9o2f.png",
 ];
@@ -12,7 +10,14 @@ export default function HomePage() {
     <main>
       {" "}
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
+        {[
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+          ...mockImages,
+        ].map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
           </div>
