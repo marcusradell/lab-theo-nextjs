8c5190a632f0066aad35a80ec6f011bfe3b07ec1
Marcus Rådell <marcus@radell.net>
2024-07-20 17:10:29 +0200

    add posts to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/86ec96a7-93ce-4b81-9d1f-d956f9fe11a4-5ffa44.png",
   "https://utfs.io/f/9e26a86e-87c5-467e-bf41-958d310b9e4a-kw9o2f.png",
@@ -5,10 +7,14 @@ const mockUrls = [
 
 const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main>
-      {" "}
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
       <div className="flex flex-wrap gap-4">
         {[
           ...mockImages,
@@ -17,8 +23,8 @@ export default function HomePage() {
           ...mockImages,
           ...mockImages,
           ...mockImages,
-        ].map((image) => (
-          <div key={image.id} className="w-48">
+        ].map((image, index) => (
+          <div key={index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
