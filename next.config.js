80a5626b1f4d9535875b509545175dda15eb496a
Marcus Rådell <marcus@radell.net>
2024-07-20 21:51:15 +0200

    try to add settings for CSP

--- next.config.js
+++ next.config.js
@@ -4,7 +4,27 @@
  */
 await import("./src/env.js");
 
+const cspHeader = `
+  default-src: 'self';
+  script-src: 'self' 'unsafe-inline' 'unsafe-eval' https://warm-lobster-4.clerk.accounts.dev;
+  connect-src: 'self' https://warm-lobster-4.clerk.accounts.dev;
+  img-src: 'self' https://img.clerk.com;
+  worker-src: 'self' blob:;
+  style-src: 'self' 'unsafe-inline';
+`;
+
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    async headers() {
+        return [
+            {source: "/(.*)",
+                headers: [
+                  {
+                    key: "Content-Security-Policy",
+                    value: cspHeader.replace(/\n/g, ""),
+                  },}
+        ]
+    }
+};
 
 export default config;
