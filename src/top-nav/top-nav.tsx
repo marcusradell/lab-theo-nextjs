86eb64ed96f1807171f19f50bcfa3cc82ef7f705
Marcus Rådell <marcus@radell.net>
2024-07-22 13:17:44 +0200

    add clientside reload for image upload

--- src/top-nav/top-nav.tsx
+++ src/top-nav/top-nav.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
@@ -11,7 +16,12 @@ export function TopNav() {
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton endpoint="imageUploader" />
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
